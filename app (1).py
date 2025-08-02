479df21eb8bc47e7fd65d8d06b06e80c72f59da9
Sheeva932 <137410421+Sheeva932@users.noreply.github.com>
2025-08-02 15:05:28 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -13,6 +13,12 @@ tfidf = joblib.load('tfidf_vectorizer.pkl')
 tfidf_matrix = joblib.load('tfidf_matrix.pkl')
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
+# Fungsi fuzzy matching
+def find_best_match(user_input):
+    titles = df_all['title'].str.lower().tolist()
+    matches = get_close_matches(user_input.lower(), titles, n=1, cutoff=0.5)
+    return matches[0] if matches else None
+
 # Fungsi untuk koreksi input judul
 def find_best_match(user_input):
     titles = df_all['title'].str.lower().tolist()
